e26f3f351be6732f06df9026884222edb72303c7
Rodrigo Moguillansky <romoguill@gmail.com>
2026-05-18 09:17:07 -0300

    First commit

---
+++ cmd/server.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"context"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+func main() {
+	// Set up logger
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	logger.Info("Starting server")
+
+	server := &http.Server{
+		Addr: ":8080",
+		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusOK)
+			w.Write([]byte("Hello, World!"))
+		}),
+	}
+
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil && err != http.ErrServerClosed {
+			logger.Error("Failed to start server", "error", err)
+			os.Exit(1)
+		}
+	}()
+
+	logger.Info("Server started on port 8080", "addr", server.Addr)
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt, syscall.SIGTERM)
+
+	<-quit
+	logger.Info("Shutting down server")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		logger.Error("Failed to shutdown server within timeout", "error", err)
+		os.Exit(1)
+	}
+
+	logger.Info("Server shutdown gracefully")
+	os.Exit(0)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/romoguill/farming-manager-api
+
+go 1.26.3
